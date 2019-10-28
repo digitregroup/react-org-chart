0bc1dca65768582a019eaf35c72c8ad61624a079
Marc Dugas <marc.dugas@digitregroup.com>
2019-10-28 10:48:23 +0100

    Fix zoom feature

--- src/chart/render-update.js
+++ src/chart/render-update.js
@@ -5,10 +5,6 @@ module.exports = renderUpdate
 // Update the rendered node positions triggered by zoom
 function renderUpdate({ svg }) {
   return () => {
-    svg.attr(
-      'transform',
-      `translate(${d3.event.translate})
-     scale(${d3.event.scale.toFixed(1)})`
-    )
+    svg.attr('transform', `translate(${d3.event.translate}) scale(${d3.event.scale})`)
   }
 }
